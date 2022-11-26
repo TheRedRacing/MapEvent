69667c0c9e9c72710c1aa9d887a3090a80ff1b40
Maxime Sickenberg <maxime.sickenberg@gmail.com>
2022-11-26 21:09:45 +0100

    fix error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,7 @@
+@extends('errors::minimal')
+
+@section('title', __('Server Error'))
+@section('code', '500')
+@section('message', __('Server Error'))
+@section('message2', __('We are already working to solve the problem.'))
+
