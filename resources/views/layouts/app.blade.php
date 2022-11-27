ef019a6e0640dce755f97d4c294872405b98d8d9
Maxime Sickenberg <maxime.sickenberg@gmail.com>
2022-11-27 14:18:20 +0100

    test Open Graph protocol

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <meta property="og:title" content="How to Become an SEO Expert (8 Steps)" />
-        <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
-        <meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />
-
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="description" content="description of your website/webpage, make sure you use keywords!" />
+
+        <meta property="og:url"                content="{{$_SERVER['SCRIPT_URI']}}" />
+        <meta property="og:type"               content="article" />
+        <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
+        <meta property="og:description"        content="How much does culture influence creative thinking?" />
+        <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
