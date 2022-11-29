c9fb94be007532a64390ff37f4b6e6e6094ee2e0
Maxime Sickenberg <maxime.sickenberg@gmail.com>
2022-11-29 19:52:53 +0100

    Responsive Login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+        
         <!-- External css -->
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
     </head>
