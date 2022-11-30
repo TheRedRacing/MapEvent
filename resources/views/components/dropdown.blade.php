c7ee5c3b05945f0f71f313d8a40ce637e4d831f0
Maxime Sickenberg <maxime.sickenberg@gmail.com>
2022-11-30 10:05:56 +0100

    Responsive Login page

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,7 +6,7 @@ switch ($align) {
         $alignmentClasses = 'origin-top-left left-0';
         break;
     case 'top':
-        $alignmentClasses = 'origin-top';
+        $alignmentClasses = 'bottom-12 right-0';
         break;
     case 'right':
     default:
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     </div>    
 
     <!-- Desktop View -->
-    <div class="hidden absolute top-5 left-5 right-5 z-20 md:flex flex-row justify-between items-center gap-5">
+    <!-- <div class="hidden absolute top-5 left-5 right-5 z-20 md:flex flex-row justify-between items-center gap-5">
         <div class="flex justify-between items-center bg-gray-950 h-12 rounded-lg text-white relative">
             <div id="toggle-menu" class="p-5 cursor-pointer">
                 <i class="fas fa-fw fa-arrow-alt-from-left"></i>
@@ -22,13 +22,23 @@
         @endif
 
         <div id="mapzoom" class="flex justify-center items-center bg-gray-950 px-5 h-12 rounded-lg text-white">{{ $mapsettings->lat }}, {{ $mapsettings->lng }}, {{ $mapsettings->zoom }}x</div>
-    </div>   
+    </div> -->  
+    
+    <!-- Mobile View -->
+    <div class="md:hidden absolute top-4 z-20 w-full px-4 flex justify-between gap-4">
+        <div class="space-x-2">
+            <button class="w-8 h-8 inline-flex justify-center items-center bg-gray-800 bg-opacity-25 rounded-full"><i class="fas fa-fw fa-user"></i></button>
+            <button class="w-8 h-8 inline-flex justify-center items-center bg-gray-800 bg-opacity-25 rounded-full"><i class="fas fa-fw fa-search"></i></button>
+        </div>
+        <div id="mapzoom" class="flex-grow flex justify-center items-center bg-gray-950 px-5 h-8 rounded-3xl text-white">{{ $mapsettings->lat }}, {{ $mapsettings->lng }}, {{ $mapsettings->zoom }}x</div>
+        <button class="w-8 h-8 inline-flex justify-center items-center bg-gray-800 bg-opacity-25 rounded-full"><i class="fas fa-fw fa-cog"></i></button>
+    </div>
 
     <div id="map" getdata="true" defaultView="{{ $mapsettings->lat }}, {{ $mapsettings->lng }}, {{ $mapsettings->zoom }}x" class="w-full h-screen z-10"></div>
     <div class="absolute bottom-0 z-30 w-full bg-gray-800 rounded-t-3xl flex flex-col justify-start items-center">
         <span class="w-10 h-1 bg-gray-300 rounded-xl mt-2"></span>
         <div class="w-full flex items-center justify-start gap-4 pt-2 px-4 pb-4">
-            <span class="w-16 h-16 rounded-full bg-cover" style="background-image: url('https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2400x1600/2982fc7cc04a2e7a76172f52e7877197/photo-1561043845-2f5e09749871.jpg');"></span>
+            <div class="w-16 h-16 relative rounded-full bg-cover" style="background-image: url('https://trello-backgrounds.s3.amazonaws.com/SharedBackground/2400x1600/2982fc7cc04a2e7a76172f52e7877197/photo-1561043845-2f5e09749871.jpg');"></div>
             <div class="flex flex-col text-white">
                 <span class="text-red-500">4th December 2022 09:30 AM</span>
                 <span class="">CCLS</span>
@@ -41,10 +51,19 @@
             </a>
             <button class="inline-flex items-center justify-center gap-2 p-2 h-[38px] bg-gray-700 border border-transparent rounded-full font-semibold text-sm text-white tracking-widest hover:bg-gray-600 active:bg-gray-600 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                 <i class="fas fa-share"></i>
-            </button>
-            <button class="inline-flex items-center justify-center gap-2 p-2 h-[38px] bg-gray-700 border border-transparent rounded-full font-semibold text-sm text-white tracking-widest hover:bg-gray-600 active:bg-gray-600 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                <i class="fas fa-ellipsis-h"></i>
-            </button>   
+            </button>            
+            <x-dropdown align="top" width="48">
+                <x-slot name="trigger">
+                    <button class="inline-flex items-center justify-center gap-2 p-2 w-full h-[38px] bg-gray-700 border border-transparent rounded-full font-semibold text-sm text-white tracking-widest hover:bg-gray-600 active:bg-gray-600 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                        <i class="fas fa-ellipsis-h"></i>
+                    </button>
+                </x-slot>
+
+                <x-slot name="content">
+                    <x-dropdown-button><i class="fas fa-fw fa-bookmark"></i> Save</x-dropdown-button>
+                    <x-dropdown-button class="hover:bg-red-500"><i class="fas fa-fw fa-exclamation"></i> Report Event</x-dropdown-button>
+                </x-slot>
+            </x-dropdown>              
         </div>
     </div>
     
